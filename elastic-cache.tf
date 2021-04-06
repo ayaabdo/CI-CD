8de0d3b699c6bf83c484729024ff371fb0125cf2
Aya Abdulsamie <ayaabdo729@gmail.com>
2021-04-06 21:06:31 +0200

    failed ll mara el 35

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -2,8 +2,10 @@ resource "aws_elasticache_cluster" "el-cache" {
   cluster_id           = "mu-cluster"
   engine               = "redis"
   node_type            = "cache.t2.micro"
+  engine_version       = "3.2.10"
   num_cache_nodes      = 1
   port                 = 6379
+  parameter_group_name = "default.redis3.2"
   security_group_ids = [aws_security_group.redis_sg.id]
   subnet_group_name = aws_elasticache_subnet_group.redis.name
 }
