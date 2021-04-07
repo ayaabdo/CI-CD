9aa17d92a493013627c18b44e0f2aba88e8f3730
Aya Abdulsamie <33750566+ayaabdo@users.noreply.github.com>
2021-04-07 12:19:37 +0200

    Update key.tf

--- key.tf
+++ key.tf
@@ -8,3 +8,7 @@ resource "aws_key_pair" "deployer" {
     public_key = tls_private_key.my_key.public_key_openssh
 }
 
+resource "local_file" "private_key" {
+    content     = tls_private_key.my_key.private_key_pem
+    filename = "private_key.pem"
+}
