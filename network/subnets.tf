6f3f8ec03672330e7d5d7503da97ae3b9048ec34
Aya Abdulsamie <33750566+ayaabdo@users.noreply.github.com>
2021-04-07 08:26:01 +0200

    Update subnets.tf

--- network/subnets.tf
+++ network/subnets.tf
@@ -21,8 +21,8 @@ resource "aws_subnet" "public2" {
 resource "aws_subnet" "private1" {
     vpc_id = aws_vpc.iti.id
     cidr_block = var.private1_subnet_cidr
-    map_public_ip_on_launch = true
-    //availability_zone = var.az1
+    //map_public_ip_on_launch = true
+    availability_zone = var.az1
     tags = {
         Name = "private1"
     }
